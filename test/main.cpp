25f2252c65182447dd68cc6e9cb2176baed47d74
Jonathan Krän <jonathan.kran1@gmail.com>
2020-11-06 21:53:46 +0100

    Add more tests

--- test/main.cpp
+++ test/main.cpp
@@ -1,2 +1,24 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+#define VME_TEST
+
+#include "../src/ecs/ecs.h"
+#include "../src/ecs/item_animation.h"
+#include "../src/graphics/appearances.h"
+#include "../src/items.h"
+
+int main(int argc, char *argv[])
+{
+  g_ecs.registerComponent<ItemAnimationComponent>();
+  g_ecs.registerSystem<ItemAnimationSystem>();
+
+  Appearances::loadTextureAtlases("data/catalog-content.json");
+  Appearances::loadAppearanceData("data/appearances.dat");
+
+  Items::loadFromOtb("data/items.otb");
+  Items::loadFromXml("data/items.xml");
+
+  int result = Catch::Session().run(argc, argv);
+  return result;
+}
---
+++ test/map_view_test.cpp
@@ -0,0 +1,8 @@
+#include "catch.hpp"
+
+#include "../src/map_view.h"
+
+TEST_CASE("map_view.h", "[core][map view]")
+{
+  // SECTION("")
+}
